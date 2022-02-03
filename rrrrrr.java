fee99ea76480525636529b0d3571ec82e07810c8
ramanan1079 <ramanan3067@gamil.com>
2022-02-03 11:17:40 +0530

    todays new project2

---
+++ rrrrrr.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class rrrrrr
+{
+public static void main (String args[])
+{
+for(int i=0;i<5;i++)
+{
+System.out.print("5");
+for(int j=1;j<=5;j++)
+{
+System.out.print(5-i );
+}
+System.out.println();
+}
+}
+}
